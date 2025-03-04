24b111105cb5a69081506dad5901adeb58605e6c
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-03-04 15:52:25 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-export const bootstrap = (async) => {
-    await initMongoConnection();
-    setupServer();
+export const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
 };
 
 bootstrap();
