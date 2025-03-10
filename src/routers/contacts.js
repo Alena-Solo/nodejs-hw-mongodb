3a7a388cfc0568f9d2849651e6ea459e3b780dcd
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-03-10 20:49:52 +0100

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -11,7 +11,7 @@ import {
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.grt('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
