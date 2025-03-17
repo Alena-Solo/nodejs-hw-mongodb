3fedc2f8c17a140f0fa6092f7f5b4e8b475ded1c
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-03-17 15:57:54 +0100

    pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,42 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { SORT_ORDER } from '../constants/index.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  return await ContactsCollection.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'name',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsFilter = ContactsCollection.find();
+
+  if (filter.type) {
+    contactsFilter.where('contactType').equals(filter.type);
+  }
+  if (filter.isFavourite !== undefined) {
+    contactsFilter.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsFilter).countDocuments(),
+    contactsFilter
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, page, perPage);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
-
 export const getContactById = async (contactId) => {
   return await ContactsCollection.findById(contactId);
 };
